a928adf2c8a3a24cfd1370007a13592c7dc3e166
Alexander Vinnik <vamposht@gmail.com>
2020-06-01 23:28:04 +0300

    ints added

--- script.js
+++ script.js
@@ -2,11 +2,18 @@
 const ctx = document.querySelector("canvas").getContext("2d");
 const circle = {
   circlesArr: [],
+  working: false,
   rad: () => {
     return Math.floor(Math.random() * 100);
   },
   color: () => {
     return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+  },
+  direction: () => {
+    return Math.floor(Math.PI * Math.ceil(Math.random() * 2));
+  },
+  steps: () => {
+    return Math.floor(Math.random() * 100);
   },
   drow: function (obj) {
     ctx.beginPath();
@@ -16,27 +23,19 @@ const circle = {
     ctx.stroke();
     ctx.closePath();
   },
-  Circle: function (x, y, r, c) {
+  Circle: function (x, y, r, c, d, s) {
     this.x = x;
     this.y = y;
     this.r = r;
     this.c = c;
+    this.d = d;
+    this.s = s;
   },
   addCircle: function (x, y) {
-    let newObj = new this.Circle(x, y, this.rad(), this.color());
+    let newObj = new this.Circle(x, y, this.rad(), this.color(), this.direction(), this.steps());
     this.circlesArr.push(newObj);
     this.drow(newObj);
   },
-  direction: () => {
-    return Math.floor(Math.PI * Math.ceil(Math.random() * 2));
-  },
-  steps: () => {
-    return Math.floor(Math.random() * 50);
-  },
-  // addCircle: function (x, y) {
-  //   this.circlesArr.push(Object.create(this.protoCircle));
-  //   this.circlesArr[this.circlesArr.length - 1].createCircle(x, y);
-  // },
 };
 document.querySelector("canvas").onclick = (e) => {
   circle.addCircle(e.offsetX, e.offsetY);
